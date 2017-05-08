80999ee35b50596a8c493e43f4b2ce83aa4e02cf
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-05-08 23:51:16 +0100

    refactored data-object and data-filter files

--- js/rd-data-filter.js
+++ js/rd-data-filter.js
@@ -2,3 +2,45 @@
  * Created by Tom.Ridd on 08/05/2017.
  */
 
+function filterData(data, filter) {
+    indexFilter = textFilterToIndexFilter(data, filter);
+    return applyFilter(data, indexFilter);
+}
+
+function textFilterToIndexFilter(data, textFilter) {
+    indexFilter = {};
+    headers = data[0];
+
+    for(key in textFilter) {
+        i = headers.indexOf(key);
+        indexFilter[i] = textFilter[key];
+    }
+
+    return indexFilter;
+}
+
+function applyFilter(data, indexFilter){
+    data2 = _.clone(data);
+
+    headerRow = data2.shift();
+    filteredRows = [];
+
+    for(d in data2) {
+        datum = data2[d];
+        if(itemPassesFilter(datum, indexFilter)) {
+            filteredRows.push(datum);
+        }
+    }
+
+    filteredRows.unshift(headerRow);
+    return filteredRows;
+}
+
+function itemPassesFilter(item, filter) {
+    for(index in filter) {
+        if (item[index] !== filter[index]) {
+            return false;
+        }
+    }
+    return true;
+}
--- js/rd-data-objects.js
+++ js/rd-data-objects.js
@@ -1,3 +1,74 @@
 /**
  * Created by Tom.Ridd on 08/05/2017.
  */
+
+
+function barchartObject(filteredData, primary_column, secondary_column) {
+    dataRows = _.clone(filteredData);
+    headerRow = dataRows.shift();
+
+    if(secondary_column === '[None]') {
+        return barchartSingleObject(headerRow, dataRows, primary_column);
+    } else {
+        return barchartDoubleObject(headerRow, dataRows, primary_column, secondary_column);
+    }
+}
+
+function barchartSingleObject(headerRow, dataRows, category) {
+    valueIndex = headerRow.indexOf('Value');
+    categoryIndex = headerRow.indexOf(category);
+    categories = uniqueDataInColumn(dataRows, categoryIndex);
+
+    values = [];
+    for(c in categories) {
+        values.push(valueForCategory(dataRows, categoryIndex, valueIndex, categories[c]));
+    }
+
+    return {
+        'type':'bar',
+        'title':{'text':'Bar Chart'},
+        'xAxis':{'title':{'text':category}, 'categories':categories},
+        'yAxis':{'title':{'text':'Percentage'}},
+        'series': [{'name':category, 'data': values}]}
+}
+
+function valueForCategory(dataRows, categoryIndex, valueIndex, categoryValue) {
+    for(r in dataRows) {
+        if(dataRows[r][categoryIndex] === categoryValue) {
+            return parseFloat(dataRows[r][valueIndex]);
+        }
+    }
+    return 0;
+}
+
+function barchartDoubleObject(headerRow, dataRows, category1, category2) {
+    valueIndex = headerRow.indexOf('Value');
+    categoryIndex = headerRow.indexOf(category1);
+    categories = uniqueDataInColumn(dataRows, categoryIndex);
+
+    seriesIndex = headerRow.indexOf(category2);
+    series = uniqueDataInColumn(dataRows, seriesIndex);
+
+    seriesData = [];
+    for(s in series) {
+        seriesRows = _.filter(dataRows, function(row) { return row[seriesIndex] === series[s];});
+        values = [];
+        for(c in categories) {
+            values.push(valueForCategory(seriesRows, categoryIndex, valueIndex, categories[c]));
+        }
+        seriesData.push({'name':series[s], 'data': values});
+    }
+
+    return {
+        'type':'bar',
+        'title':{'text':'Double bar Chart'},
+        'xAxis':{'title':{'text':category1}, 'categories':categories},
+        'yAxis':{'title':{'text':'Percentage'}},
+        'series': seriesData};
+}
+
+function uniqueDataInColumn(data, index) {
+    values = _.map(data.slice(start = 1), function(item) {
+        return item[index]; });
+    return _.uniq(values).sort();
+}
